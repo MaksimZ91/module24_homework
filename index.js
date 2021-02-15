fdc8665174c6762960bc1175186dc211ad3118f5
MaksimZ91 <ak4202@mail.ru>
2021-02-15 17:04:02 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const yargs =require ("yargs")
+const fs = require('fs/promises')
+const path=require('path')
+
+
+
+const argv=yargs
+.option ('component', {alias:"c" , string : true})
+.demandOption([`component`], 'Введите тип компонента')
+.command(['create <name> [filename]', 'cr'], 'DСоздание шаблонаs', {}, (argv)=>{
+ const filepath=path.resolve(argv.filename||argv.name)
+ const content =`import  React from "react";
+ import  ${argv.component} from "./${argv.component}"
+ function ${argv.name} (){
+   return(
+     
+     <${argv.component}/>
+     <> 
+   )
+ }
+  export default ${argv.name}`
+  fs.writeFile(argv.filename||`${filepath}.js`,content||defaultcontent)
+ .then(()=>console.log("save"))
+ .catch(()=>console.error('dont save',e))
+})
+.argv
+
+  
