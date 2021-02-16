60e8fbabef5c48df597b9f0114ecbcba484a6c07
MaksimZ91 <ak4202@mail.ru>
2021-02-16 11:56:17 +0800

    comp

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const argv=yargs
 .demandOption([`component`], 'Введите тип компонента')
 .command(['create <name> [filename]', 'cr'], 'Создание шаблона', {}, (argv)=>{
  const filepath=path.resolve(argv.filename||argv.name)
+ if (argv.component)
  const content =`import  React from "react";
  import  ${argv.component} from "./${argv.component}"
  function ${argv.name} (){
@@ -19,7 +20,7 @@ const argv=yargs
    )
  }
   export default ${argv.name}`
-  fs.writeFile(argv.filename||`${filepath}.js`,content||defaultcontent)
+  fs.writeFile(argv.filename||`${filepath}.js`,content)
  .then(()=>console.log("save"))
  .catch(()=>console.error('dont save',e))
 })
