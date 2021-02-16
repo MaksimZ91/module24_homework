c9c2d84398e2d71980938223415c3b22b04fcac4
MaksimZ91 <ak4202@mail.ru>
2021-02-16 12:25:05 +0800

    comp

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const fs = require('fs/promises')
 const path=require('path')
 
 
-
- 
-
-
 const argv=yargs
 .option ('component', {alias:"c" , string : true})
 .demandOption([`component`], 'Введите тип компонента')
-.command(['create <name> [filename]', 'cr'], 'Создание шаблона', {}, function (argv){
+.command(['create <name> [filename]', 'cr'], 'Создание шаблона', {}, (argv)=>{
  const filepath=path.resolve(argv.filename||argv.name)
  const content =`import  React from "react";
   function ${argv.name} (){
@@ -21,7 +17,7 @@ const argv=yargs
    )
  }
   export default ${argv.name}`
-  fs.writeFile(argv.filename||`${filepath}.js`,content)
+  fs.writeFile(`${filepath}.js`,content)
  .then(()=>console.log("save"))
  .catch(()=>console.error('dont save',e))
 })
